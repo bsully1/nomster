2f9ea632e5a8fba395a21d9811a5a3273ed88ed2
Brandon Sullivan <jbs81890@gmail.com>
2019-09-11 17:54:57 +0000

    Added a new page for new places to be submitted and gave that new page the booyah bar then gave it name,address,description and a boostrap create button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all.page(params[:page])
+    @places = Place.all.paginate(page: params[:page], per_page: 1)
+  end
+
+  def new
+    @place = Place.new
   end
 end
