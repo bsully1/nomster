8df00f3760d36233ec5789bdefcd461d23bfc709
Brandon Sullivan <jbs81890@gmail.com>
2019-09-24 19:10:12 +0000

    Photo page created and was working, pushing to find error

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,6 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:comment).permit(:message, :rating)
+    params.require(:photo).permit(:caption)
   end
 end
