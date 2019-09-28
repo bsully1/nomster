df8d9c61f0c302fa00c4d50d28f18f99c6ef0e4f
Brandon Sullivan <jbs81890@gmail.com>
2019-09-28 15:19:47 +0000

    Finished the initial email setup

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
 
     def comment_added
-      mail(to: "jbs81890@gmail.com",
+      mail(to: "brandon.sullivan81890@gmail.com",
         subject: "A comment has been added to your place")
     end
 end
